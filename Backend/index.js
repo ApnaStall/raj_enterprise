a332630ce6991733877521cc77b2adc69e374194
Dax <apnastall.project@gmail.com>
2025-12-21 17:48:02 +0530

    Initial Commit

--- Backend/index.js
+++ Backend/index.js
@@ -19,12 +19,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
