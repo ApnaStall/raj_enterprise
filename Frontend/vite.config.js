ec3f14b01902382d58214a16092892f7d0ca8e2b
Dax <apnastall.project@gmail.com>
2025-12-22 10:19:52 +0530

    Refresh Fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000, // increase limit to silence warning
+  },
 })
